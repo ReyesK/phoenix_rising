52aa83c5fd363d3716490035c5410e8fafc4504a
Kyle Reyes <mr.kylereyes@gmail.com>
2020-11-08 11:32:08 -0500

    update style

--- js/phoenix-rising.js
+++ js/phoenix-rising.js
@@ -3,10 +3,25 @@
     var script = document.createElement("script");
     script.src = "https://code.jquery.com/jquery-3.5.1.min.js";
     script.type = "text/javascript";
-    script.onload = function() {
+    script.onload = function() { // do jquery stuff here
         var $ = window.jQuery;
-        $(document).ready(function(){ // do jquery stuff here
-          $('.sidebar--left > .sidebar__content').height(window.innerHeight - 140);
+
+
+        $(document).ready(function(){
+          $('.sidebar__content').height(window.innerHeight - 140);
+
+          if ($('.sidebar--right > .sidebar__content').children().length === 0) {
+            $('.sidebar--left > .sidebar__content').show();
+          } else {
+            $('.sidebar__content').show();
+          }
+
+          $('.sidebar__content').css({
+            backgroundColor: '#f7f9fb',
+            marginTop: '20px',
+            borderRadius: '4px',
+            padding: '10px 5px 10px 10px'
+          });
         });
     };
     document.getElementsByTagName("head")[0].appendChild(script);
